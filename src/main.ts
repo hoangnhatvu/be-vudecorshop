88ba3028c4dcabb7d300d199d6fc1d77eaa0eb5b
Vũ <hoangnhatvu35202@gmail.com>
2024-07-16 00:15:56 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
